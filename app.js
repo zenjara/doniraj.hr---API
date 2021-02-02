9d5c755a2fd403f21d8122e88baccf6d75a7eb01
Ivan Matas <ivan.matas@arsfutura.co>
2021-02-02 20:56:56 +0100

    add proocfile

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use(
 
 ////////
 
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
+app.listen(process.env.PORT || 3000, () => {
+    console.log('App running ...')
 })
 
